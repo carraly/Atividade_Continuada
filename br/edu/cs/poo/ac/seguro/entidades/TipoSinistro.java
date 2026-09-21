016fc0dcf0dd10a5f47bf6359faabf90aef851bc
Victor Carraly <vcarraly@gmail.com>
2026-09-21 16:40:21 -0300

    Adiciona gitignore, src e começa TipoSinistro.java

--- br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
+++ br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
@@ -14,4 +14,29 @@ package br.edu.cs.poo.ac.seguro.entidades;
  * retorna o tipo de sinistro correspondente ao código recebido como parâmetro
  */
 public enum TipoSinistro {
+	COLISAO(1,"Colisão"),
+	INCENDIO(2,"Incêndio"),
+	FURTO(3, "Furto"),
+	ENCHENTE(4, "Enchente"),
+	DEPREDACAO(5, "Depredação");
+
+	private final int codigo;
+	private final String nome;
+	
+	private TipoSinistro(int codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+
+	public void getCodigo() {
+			
+	}
+
+	public void getNome() {
+
+	}
+
+	public static TipoSinistro getTipoSinistro(int codigo) {
+
+	}
 }
