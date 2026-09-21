79a37634774dbe942c8682d12027b976e735bf86
Victor Carraly <vcarraly@gmail.com>
2026-09-21 15:53:46 -0300

    Cria pacote e adiciona as classes fornecidas

---
+++ br/edu/cs/poo/ac/seguro/entidades/TipoSinistro.java
@@ -0,0 +1,17 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+/*
+ * Implementar um enum com as seguintes constantes:
+ * 
+ * 	COLISAO(1,"Colisão"),
+	INCENDIO(2,"Incêndio"),
+	FURTO(3, "Furto"),
+	ENCHENTE(4, "Enchente"),
+	DEPREDACAO(5, "Depredação");
+ * 
+ * O enum deve ter construtor privado, métodos get públicos para os atributos codigo e nome,
+ * e um método público e estático TipoSinistro getTipoSinistro(int codigo), que 
+ * retorna o tipo de sinistro correspondente ao código recebido como parâmetro
+ */
+public enum TipoSinistro {
+}
