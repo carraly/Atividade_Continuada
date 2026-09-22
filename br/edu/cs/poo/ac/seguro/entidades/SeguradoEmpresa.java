558b2ed760ec00f2cc69821224298f27154543c5
Victor Carraly <vcarraly@gmail.com>
2026-09-22 19:31:11 -0300

    Arruma a identação em SeguradoEmpresa.java

--- br/edu/cs/poo/ac/seguro/entidades/SeguradoEmpresa.java
+++ br/edu/cs/poo/ac/seguro/entidades/SeguradoEmpresa.java
@@ -2,46 +2,46 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class SeguradoEmpresa extends Segurado{
-        private String cnpj;
-        private double faturamento;
-        private boolean ehLocadoraDeVeiculos;
+    private String cnpj;
+    private double faturamento;
+    private boolean ehLocadoraDeVeiculos;
 
-        public SeguradoEmpresa(String nome, Endereco endereco, LocalDate dataCriacao, BigDecimal bonus, String cnpj, double faturamento, boolean ehLocadoraDeVeiculos) {
-            super(nome, endereco, dataCriacao, bonus);
-            this.cnpj = cnpj;
-            this.faturamento = faturamento;
-            this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
-        }
+    public SeguradoEmpresa(String nome, Endereco endereco, LocalDate dataCriacao, BigDecimal bonus, String cnpj, double faturamento, boolean ehLocadoraDeVeiculos) {
+        super(nome, endereco, dataCriacao, bonus);
+        this.cnpj = cnpj;
+        this.faturamento = faturamento;
+        this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
+    }
 
-        public String getCnpj() {
-            return cnpj;
-        }
+    public String getCnpj() {
+        return cnpj;
+    }
 
-        public void setCnpj(String cnpj) {
-            this.cnpj = cnpj;
-        }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 
-        public double getFaturamento() {
-            return faturamento;
-        }
+    public double getFaturamento() {
+        return faturamento;
+    }
 
-        public void setFaturamento(double faturamento) {
-            this.faturamento = faturamento;
-        }
+    public void setFaturamento(double faturamento) {
+        this.faturamento = faturamento;
+    }
 
-        public Boolean getEhLocadoraDeVeiculos() {
-            return ehLocadoraDeVeiculos;
-        }
+    public Boolean getEhLocadoraDeVeiculos() {
+        return ehLocadoraDeVeiculos;
+    }
 
-        public void setEhLocadoraDeVeiculos(Boolean ehLocadoraDeVeiculos) {
-            this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
-        }
+    public void setEhLocadoraDeVeiculos(Boolean ehLocadoraDeVeiculos) {
+        this.ehLocadoraDeVeiculos = ehLocadoraDeVeiculos;
+    }
 
-        public LocalDate getDataAbertura() {
-            return super.getDataCriacao();
-        }
+    public LocalDate getDataAbertura() {
+        return super.getDataCriacao();
+    }
 
-        public void setDataAbertura(LocalDate dataAbertura) {
-            super.setDataCriacao(dataAbertura);
-        }
+    public void setDataAbertura(LocalDate dataAbertura) {
+        super.setDataCriacao(dataAbertura);
+    }
 }
