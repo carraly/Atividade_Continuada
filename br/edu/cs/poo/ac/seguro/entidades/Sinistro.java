c35d0bb7c6170860bd5cb8748d78f68bcedc7e20
Victor Carraly <vcarraly@gmail.com>
2026-09-22 19:54:11 -0300

    Corrige Apolice, Sinistro e Veiculo que eu havia entendido errado

--- br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
+++ br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
@@ -1,3 +1,12 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Sinistro {
     private String numero;
     private Veiculo veiculo;
@@ -6,71 +15,4 @@ public class Sinistro {
     private String usuarioRegistro;
     private BigDecimal valorSinistro;
     private TipoSinistro tipo;
-    
-    public Sinistro(String numero, Veiculo veiculo, LocalDateTime dataHoraSinistro, LocalDateTime dataHoraRegistro,
-            String usuarioRegistro, BigDecimal valorSinistro, TipoSinistro tipo) {
-        this.numero = numero;
-        this.veiculo = veiculo;
-        this.dataHoraSinistro = dataHoraSinistro;
-        this.dataHoraRegistro = dataHoraRegistro;
-        this.usuarioRegistro = usuarioRegistro;
-        this.valorSinistro = valorSinistro;
-        this.tipo = tipo;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public Veiculo getVeiculo() {
-        return veiculo;
-    }
-
-    public void setVeiculo(Veiculo veiculo) {
-        this.veiculo = veiculo;
-    }
-
-    public LocalDateTime getDataHoraSinistro() {
-        return dataHoraSinistro;
-    }
-
-    public void setDataHoraSinistro(LocalDateTime dataHoraSinistro) {
-        this.dataHoraSinistro = dataHoraSinistro;
-    }
-
-    public LocalDateTime getDataHoraRegistro() {
-        return dataHoraRegistro;
-    }
-
-    public void setDataHoraRegistro(LocalDateTime dataHoraRegistro) {
-        this.dataHoraRegistro = dataHoraRegistro;
-    }
-
-    public String getUsuarioRegistro() {
-        return usuarioRegistro;
-    }
-
-    public void setUsuarioRegistro(String usuarioRegistro) {
-        this.usuarioRegistro = usuarioRegistro;
-    }
-
-    public BigDecimal getValorSinistro() {
-        return valorSinistro;
-    }
-
-    public void setValorSinistro(BigDecimal valorSinistro) {
-        this.valorSinistro = valorSinistro;
-    }
-
-    public TipoSinistro getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoSinistro tipo) {
-        this.tipo = tipo;
-    }
 }
--- br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
+++ br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
@@ -1,49 +1,14 @@
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Veiculo {
     private String placa;
     private int ano;
     private SeguradoEmpresa proprietarioEmpresa;
     private SeguradoPessoa proprietarioPessoa;
     private CategoriaVeiculo categoria;
-
-    public Veiculo(String placa, int ano, SeguradoEmpresa proprietarioEmpresa, SeguradoPessoa proprietarioPessoa,
-            CategoriaVeiculo categoria) {
-        this.placa = placa;
-        this.ano = ano;
-        this.proprietarioEmpresa = proprietarioEmpresa;
-        this.proprietarioPessoa = proprietarioPessoa;
-        this.categoria = categoria;
-    }
-    
-    public String getPlaca() {
-        return placa;
-    }
-    public void setPlaca(String placa) {
-        this.placa = placa;
-    }
-    public int getAno() {
-        return ano;
-    }
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-    public SeguradoEmpresa getProprietarioEmpresa() {
-        return proprietarioEmpresa;
-    }
-    public void setProprietarioEmpresa(SeguradoEmpresa proprietarioEmpresa) {
-        this.proprietarioEmpresa = proprietarioEmpresa;
-    }
-    public SeguradoPessoa getProprietarioPessoa() {
-        return proprietarioPessoa;
-    }
-    public void setProprietarioPessoa(SeguradoPessoa proprietarioPessoa) {
-        this.proprietarioPessoa = proprietarioPessoa;
-    }
-    public CategoriaVeiculo getCategoria() {
-        return categoria;
-    }
-    public void setCategoria(CategoriaVeiculo categoria) {
-        this.categoria = categoria;
-    }
-
-    
 }
