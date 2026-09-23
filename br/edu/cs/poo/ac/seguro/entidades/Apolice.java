8733a467aef429252736d3cde5e7c78990b7a2fc
Victor Carraly <vcarraly@gmail.com>
2026-09-23 18:33:35 -0300

    Adiciona String numero em Apolice.java e modifica construtor para não inicializa-lo, também remove o import de AllArgsConstructor desse arquivo e de Sinistro.java

--- br/edu/cs/poo/ac/seguro/entidades/Apolice.java
+++ br/edu/cs/poo/ac/seguro/entidades/Apolice.java
@@ -1,14 +1,20 @@
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class Apolice {
+    private String numero;
     private Veiculo veiculo;
     private BigDecimal valorFranquia;
     private BigDecimal valorPremio;
     private BigDecimal valorMaximoSegurado;
+
+    public Apolice(Veiculo veiculo, BigDecimal valorFranquia, BigDecimal valorPremio, BigDecimal valorMaximoSegurado) {
+        this.veiculo = veiculo;
+        this.valorFranquia = valorFranquia;
+        this.valorPremio = valorPremio;
+        this.valorMaximoSegurado = valorMaximoSegurado;
+    }
 }
--- br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
+++ br/edu/cs/poo/ac/seguro/entidades/Sinistro.java
@@ -2,7 +2,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
